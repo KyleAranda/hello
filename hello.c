32b116c27bd14eb9e6d2f23bfc2452fbee45b12d
wkoumaka <59955630+wkoumaka@users.noreply.github.com>
2020-01-16 19:05:42 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -4,7 +4,7 @@ int main (void) {
 
     printf("Kyle Aranda");
     printf("Electrical Engineering");
-    printf("wil Koumaka")
-    printf("computer engineering")
+    printf("wil Koumaka");
+    printf("computer engineering");
     return 0;
 }
